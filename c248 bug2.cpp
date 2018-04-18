c96cdef019545862ccde3031350694f45ea5690d
vincent97198 <31513765+vincent97198@users.noreply.github.com>
2018-04-18 20:49:15 +0800

    Update c248 bug2.cpp

--- c248 bug2.cpp
+++ c248 bug2.cpp
@@ -1,70 +1,73 @@
 #include <iostream>
 #include <map>
 #include <vector>
-#define mm 1000000007
-#define N 1048576
+
 
 using namespace std;
 
-int main()
+int mm=1000000007;
+int N=9999;
+int ans[1048576+1];
+bool primeTest[1048576+1];
+
+int main(void)
 {
 	int t;
-	
+
 	while(cin >> t)
 	{
 		int n;
-		int ans[N+1];
+
 		map<int,int> son;
 		vector<int> prime;
-		bool primeTest[N+1];
-		
+
 		ans[0]=1;
-		
+
 		for(int i=1;i<=N;i++)
 			primeTest[i]=true;
-			
+
 		for(int i=2;i<=N;i++)
 		{
 			if(primeTest[i])
 			{
 				for(int j=i*2;j<=N;j+=i)
 					primeTest[i]=false;
-					
+
 				prime.push_back(i);
 			}
 		}
-		
+
 		for(int i=1;i<=N;i++)
 		{
 			int tmp=4*i-2,tmp2=i+1;
-			int ansTmp=1;
-		
+			long long int ansTmp=1;
+
 			vector<int>::iterator iter=prime.begin();
-			
-			while(tmp>1)
+
+			while(tmp>1 && iter!=prime.end())
 			{
 				if(tmp%*iter==0)
 				{
 					tmp/=*iter;
 					son[*iter]++;
 				}
-				
-				iter++;
+				else
+                    iter++;
 			}
-			
+
 			iter=prime.begin();
-			
-			while(tmp2>1)
+
+			while(tmp2>1 && iter!=prime.end())
 			{
 				if(tmp2%*iter==0)
 				{
 					tmp2/=*iter;
 					son[*iter]--;
 				}
-				
-				iter++;
+				else
+                    iter++;
 			}
-			
+
 			for(map<int,int>::iterator iter2=son.begin();iter2!=son.end();iter2++)
 			{
 				for(int i=iter2->second;i>0;i--)
@@ -72,15 +75,15 @@ int main()
 					ansTmp=(ansTmp*iter2->first)%mm;
 				}
 			}
-			
+
 			ans[i]=ansTmp;
 		}
-		
+
 		while(cin >> n && t--)
 		{
 			cout << ans[n] << endl;
 		}
 	}
-	
+
 	return 0;
 }
